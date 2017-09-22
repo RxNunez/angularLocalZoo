2742757b468d9df91d0dbadbb01130be1e230ee3
Maria Thomas <malethr@gmail.com>
2017-09-22 12:04:04 -0700

    add initial images and add initial constructor for animal

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1 class="jumbotron">ZOOmAPP</h1>
-    <div class="row well">
+    <div class="row">
       <div class="col-md-10">
         <label>View as:</label>
         <select (change)="onChange($event.target.value)">
@@ -17,6 +17,13 @@ import { Component } from '@angular/core';
       <div class="col-md-2">
         <button class="btn btn-default">Add New Animal<span class="add-plus">&nbsp;+</span></button>
       </div>
+      <div class="well" *ngFor="let currentAnimal of animals">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="">
+            <h3></h3>
+          </div>
+      </div>
     </div>
   </div>
   `
@@ -24,4 +31,13 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
+}
+
+export class Animal {
+  public dateNow: Date;
+  public age: number;
+  constructor(public images: string, public species: string, public name: string, public bday: Date, public diet: string, public location: string, public caretakers: number, public sex: string, public likes:string, public dislikes: string){
+    this.dateNow = new Date();
+    this.age = this.dateNow.getFullYear()-bday.getFullYear();
+  }
 }
