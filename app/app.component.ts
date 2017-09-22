9fb5ad0de03325c1b1d398b794065d3a397f2d70
Maria Thomas <malethr@gmail.com>
2017-09-22 15:44:00 -0700

    add edit form

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,25 +18,50 @@ import { Component } from '@angular/core';
         <button class="btn btn-default btn-lg"><span class="add-plus">&nbsp;+<br></span>Add New Animal</button>
       </div>
     </div>
-      <div class="well" *ngFor="let currentAnimal of masterAnimals">
-        <div class="row">
-          <div class="col-md-4">
-            <img src="{{currentAnimal.images}}">
-            <p>{{currentAnimal.species}}</p>
-          </div>
-          <div class="col-md-6">
-            <h3>&nbsp;{{currentAnimal.name}}</h3>
-            <p>Age:&nbsp;{{getAge(currentAnimal.bday)}}</p>
-            <p>Birthday:&nbsp;{{currentAnimal.bday}}</p>
-            <p>Sex:&nbsp;{{currentAnimal.sex}}</p>
-            <p>Number of caretakers:&nbsp;{{currentAnimal.caretakers}}</p>
-            <p>Location:&nbsp;{{currentAnimal.location}}</p>
-            <p>Likes:&nbsp;{{currentAnimal.likes}}</p>
-            <p>Dislikes:&nbsp;{{currentAnimal.dislikes}}</p>
-          </div>
-          <div class="col-md-2">
-            <button class="btn btn-success btn-lg">EDIT</button>
-          </div>
+    <hr>
+
+    <div class="form-group row">
+      <div class="col-md-4">
+        <img src={{selectedAnimal.image}}>
+        <h3>{{selectedAnimal.species}}</h3>
+        <label>Edit image URL:</label>
+        <input type="url" class="form-control" [(ngModel)]="selectedAnimal.image" placeholder="paste URL">
+        <label>Edit name:</label>
+        <input class="form-control"[(ngModel)]="selectedAnimal.name">
+        <label>Edit location:</label>
+        <input class="form-control"[(ngModel)]="selectedAnimal.location">
+        <label>Edit sex:</label>
+        <input class="form-control"[(ngModel)]="selectedAnimal.sex">
+        <label>Edit caretakers:</label>
+        <input class="form-control"[(ngModel)]="selectedAnimal.caretakers">
+        <label>Edit likes:</label>
+        <input class="form-control"[(ngModel)]="selectedAnimal.likes">
+        <label>Enter dislikes:</label>
+        <input class="form-control"[(ngModel)]="selectedAnimal.dislikes">
+      </div>
+    </div>
+
+    <div class="well" *ngFor="let currentAnimal of masterAnimals">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="{{currentAnimal.image}}">
+          <p>{{currentAnimal.species}}</p>
+        </div>
+        <div class="col-md-4">
+          <h3>&nbsp;{{currentAnimal.name}}</h3>
+          <p>Age:&nbsp;{{getAge(currentAnimal.bday)}}</p>
+          <p>Birthday:&nbsp;{{currentAnimal.bday}}</p>
+          <p>Sex:&nbsp;{{currentAnimal.sex}}</p>
+          <p>Number of caretakers:&nbsp;{{currentAnimal.caretakers}}</p>
+
+        </div>
+        <div class="col-md-4">
+          <p>Location:&nbsp;{{currentAnimal.location}}</p>
+          <p>Likes:&nbsp;{{currentAnimal.likes}}</p>
+          <p>Dislikes:&nbsp;{{currentAnimal.dislikes}}</p>
+          <br><br>
+          <button class="btn btn-success btn-lg" (click)="editAnimal()">Edit!</button>
+        </div>
       </div>
     </div>
   </div>
@@ -49,26 +74,30 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   public age: number;
+  public birthDates;
 
 
   masterAnimals: Animal[] = [
     new Animal('/resources/images/arcticFox.jpg','Arctic Fox','Moon','10-13-2016','Carnivore','Northern Trail',5,'Female','Cool shade','Loud Noises'),
-    new Animal('/resources/images/northwestBlackTailedDeer.jpg','Northwest Black Tailed Deer','Moon','11-25-2009','Carnivore','Northern Trail',5,'Female','Cool shade','Loud Noises'),
-    new Animal('/resources/images/ocelot.jpg','Ocelot','Moon','01-01-2013','Carnivore','Northern Trail',5,'Female','Cool shade','Loud Noises'),
+    new Animal('/resources/images/northwestBlackTailedDeer.jpg','Northwest Black Tailed Deer','Prince','11-25-2009','Carnivore','Northern Trail',5,'Male','Cool shade','Loud Noises'),
+    new Animal('/resources/images/ocelot.jpg','Ocelot','Tinkerbell','01-01-2013','Carnivore','Northern Trail',5,'Female','Cool shade','Loud Noises'),
   ];
 
-  public birthDates;
+    selectedAnimal: Animal = this.masterAnimals[0];
 
   getAge(bday){
     this.birthDates = new Date(bday);
     return this.age = this.currentTime.getFullYear()-this.birthDates.getFullYear();
-
   }
+
+  editAnimal(){
+    alert("edit cliked")
+  }
+
 }
 
 export class Animal {
 
-  constructor(public images: string, public species: string, public name: string, public bday: string, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string){
-
+  constructor(public image: string, public species: string, public name: string, public bday: string, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string){
   }
 }
