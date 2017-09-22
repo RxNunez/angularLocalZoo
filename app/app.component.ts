936109f2cfaa9ddf45ea669b6dd47306e7aef8ee
Maria Thomas <malethr@gmail.com>
2017-09-22 11:17:11 -0700

    add view as selection option and add new button

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1 class="jumbotron">ZOOmAPP</h1>
+    <div class="row well">
+      <div class="col-md-10">
+        <label>View as:</label>
+        <select (change)="onChange($event.target.value)">
+          <option selected="selected" value="all">All Animals</option>
+          <option value="young">All Young Animals</option>
+          <option value="old">All Old Animals</option>
+        </select>
+      </div>
+      <div class="col-md-2">
+        <button class="btn btn-default">Add New Animal<span class="add-plus">&nbsp;+</span></button>
+      </div>
+    </div>
   </div>
   `
 })
